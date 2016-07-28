618e1ef448b5fae6508f141d64f0ac9f99b5d635
James <james@team-wombat.com>
2016-07-28 09:59:05 -0400

    Created game controller insides

--- public/js/game.controller.js
+++ public/js/game.controller.js
@@ -1,1 +1,29 @@
-game page
+"use strict";
+
+(function() {
+    angular
+        .module("galaga")
+        .controller("GameController", [
+            "$scope",
+            GameControllerFunction
+        ])
+
+    function GameControllerFunction($scope) {
+        var socket = io();
+        var vm = this;
+
+
+        // vm.sendMessage = function() {
+        //     socket.emit('chat message', vm.newMessage);
+        //     vm.newMessage = "";
+        // }
+        //
+        // socket.on('chat message', function(msg) {
+        //     $scope.$apply(function() {
+        //         vm.messages.push(msg);
+        //     })
+        // });
+
+    }
+
+})();
